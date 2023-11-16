5448f9f4c1d21a9814b062fcc9a08a6eb5a4340b
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-11-16 18:34:42 -0500

    Fix capitalization

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import session from "express-session";
 
