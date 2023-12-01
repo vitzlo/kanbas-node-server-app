aad3d5977fb184d409918462c19bf62e10619e86
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-11-30 22:24:30 -0500

    Assignment first pass

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import session from "express-session";
+
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(
+    session(sessionOptions)
+);
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
