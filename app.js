30d2f523e69192601d3aff6d6c238c6b86b75de8
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-12-04 08:46:51 -0500

    netlify FRONTEND_URL

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
 // const FRONTEND_URL = "http://localhost:3000"
-// const FRONTEND_URL = "https://radiant-kulfi-63b9ed.netlify.app/"
-const FRONTEND_URL = "https://kanbas-node-server-app-a6-m3tp.onrender.com" // ?????
+const FRONTEND_URL = "https://a6--radiant-kulfi-63b9ed.netlify.app"
+// const FRONTEND_URL = "https://kanbas-node-server-app-a6-m3tp.onrender.com" // ?????
 
 // const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
 const CONNECTION_STRING = "mongodb+srv://victor:supersecretpassword@kanbas-cluster.0u5ww1q.mongodb.net/?retryWrites=true&w=majority"
