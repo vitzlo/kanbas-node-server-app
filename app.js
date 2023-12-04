cc1cb1260b5ffbce7c984c8452c20dd829bcdae0
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-12-04 08:32:22 -0500

    Atlas connection string

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const FRONTEND_URL = "http://localhost:3000"
 // const FRONTEND_URL = "https://radiant-kulfi-63b9ed.netlify.app/"
 // const FRONTEND_URL = "https://kanbas-node-server-app-a6-m3tp.onrender.com" // ?????
 
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
-// const CONNECTION_STRING = "mongodb+srv://victor:supersecretpassword@kanbas-cluster.0u5ww1q.mongodb.net/?retryWrites=true&w=majority"
+// const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
+const CONNECTION_STRING = "mongodb+srv://victor:supersecretpassword@kanbas-cluster.0u5ww1q.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(CONNECTION_STRING);
 const app = express();
