cd3898b6ee14224625295343c71cf241e165aad7
Victor Louie <58094419+vitzlo@users.noreply.github.com>
2023-12-04 10:09:31 -0500

    install, path change

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const FRONTEND_URL = "https://a6--radiant-kulfi-63b9ed.netlify.app"
 // const FRONTEND_URL = "https://kanbas-node-server-app-a6-m3tp.onrender.com" // ?????
 
 // const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kanbas"
-const CONNECTION_STRING = "mongodb+srv://victor:supersecretpassword@kanbas-cluster.0u5ww1q.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_STRING = "mongodb+srv://victor:supersecretpassword@kanbas-cluster.0u5ww1q.mongodb.net/kanbas?retryWrites=true&w=majority"
 mongoose.connect(CONNECTION_STRING);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
